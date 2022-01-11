73bf2fe487d8ad9e38238fa4c06a7a0fd5458e28
taylanot <taylanozgurturan@gmail.com>
2022-01-11 17:29:45 +0100

    Still not working?

--- setup.py
+++ setup.py
@@ -8,3 +8,12 @@ setup(
    author_email='taylanozgurturan@gmail.com',
    packages=setuptools.find_packages()
 )
+
+setup(
+   name='SolidMechanicsTools',
+   version='0.0.1',
+   description='Solid Mechanics Toolbox for FEniCS',
+   author='O.T. Turan',
+   author_email='taylanozgurturan@gmail.com',
+   packages=['SolidMechanicsTools'],
+)
