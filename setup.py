2bce956aef0a5dcf0a91eefdc6ff7b723200aec3
taylanot <taylanozgurturan@gmail.com>
2022-01-11 21:34:36 +0100

    Try MANIFEST.in

--- setup.py
+++ setup.py
@@ -1,24 +1,25 @@
 from setuptools import setup
 import os 
-#setup(
-#   name='SolidMechanicsTools',
-#   version='0.0.1',
-#   description='Solid Mechanics Toolbox for FEniCS',
-#   author='O.T. Turan',
-#   author_email='taylanozgurturan@gmail.com',
-#   packages=setuptools.find_packages()
-#)
-
 setup(
    name='SolidMechanicsTools',
    version='0.0.1',
    description='Solid Mechanics Toolbox for FEniCS',
    author='O.T. Turan',
    author_email='taylanozgurturan@gmail.com',
-   packages=['SolidMechanicsTools'],
-   package_data={
-       'SolidMechanicsTools': [os.path.join('materials','*.py'),\
-               os.path.join('src','*.py'),os.path.join('models','*.py')],
-       },
-
+   packages=setuptools.find_packages(),
+   include_package_data=True,
 )
+
+#setup(
+#   name='SolidMechanicsTools',
+#   version='0.0.1',
+#   description='Solid Mechanics Toolbox for FEniCS',
+#   author='O.T. Turan',
+#   author_email='taylanozgurturan@gmail.com',
+#   packages=['SolidMechanicsTools'],
+#   package_data={
+#       'SolidMechanicsTools': [os.path.join('materials','*.py'),\
+#               os.path.join('src','*.py'),os.path.join('models','*.py')],
+#       },
+#
+#)
