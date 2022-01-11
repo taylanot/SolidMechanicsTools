3e486756374ee93be641d459bc9c22b2ba0878d1
taylanot <taylanozgurturan@gmail.com>
2022-01-11 17:32:50 +0100

    Yeap Working

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
-setup(
-   name='SolidMechanicsTools',
-   version='0.0.1',
-   description='Solid Mechanics Toolbox for FEniCS',
-   author='O.T. Turan',
-   author_email='taylanozgurturan@gmail.com',
-   packages=setuptools.find_packages()
-)
+#setup(
+#   name='SolidMechanicsTools',
+#   version='0.0.1',
+#   description='Solid Mechanics Toolbox for FEniCS',
+#   author='O.T. Turan',
+#   author_email='taylanozgurturan@gmail.com',
+#   packages=setuptools.find_packages()
+#)
 
 setup(
    name='SolidMechanicsTools',
